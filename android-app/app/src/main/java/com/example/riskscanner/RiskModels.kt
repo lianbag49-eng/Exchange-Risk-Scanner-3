038e38f1d672bf75223d8f7d67d435123b563e3d
lianbag49-eng <lianbag49@gmail.com>
2026-09-14 22:14:05 +0900

    Add risk models

---
+++ android-app/app/src/main/java/com/example/riskscanner/RiskModels.kt
@@ -0,0 +1,17 @@
+package com.example.riskscanner
+
+data class RiskSignal(val label: String, val value: String, val points: Int, val triggered: Boolean)
+data class RiskResult(val score: Int, val level: String, val signals: List<RiskSignal>)
+data class DeviceSnapshot(
+    val deviceCountry: String,
+    val timezoneId: String,
+    val locale: String,
+    val networkType: String,
+    val vpnActive: Boolean,
+    val rootedSuspected: Boolean,
+    val emulatorSuspected: Boolean,
+    val developerOptions: Boolean,
+    val adbEnabled: Boolean,
+    val secureLockScreen: Boolean,
+    val securityPatch: String
+)
